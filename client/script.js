ffb8986cfe347812e19bbfa34f681c058b9d40e3
ykfuku <ykfuku@hotmail.co.jp>
2014-12-03 14:42:53 +0900

    新規作成
    新規作成

---
+++ client/script.js
@@ -0,0 +1,21 @@
+﻿var MainApp = angular.module('MainApp', ['ngRoute']);
+
+MainApp.config(function($routeProvider){
+    $routeProvider.
+    when('/', { 
+        // ログイン画面を表示
+        controller : 'mainsCtrl',
+        templateUrl: 'login.html',
+    }).
+    when('/view2',{
+        // 
+        controller : 'mainsCtrl',
+        templateUrl: 'view2.html',
+    }).
+    otherwise({ // 上記に該当しない場合は '/' にリダイレクトしてログイン画面を表示
+        redirectTo: '/'
+    });
+});
+
+MainApp.controller('mainsCtrl', function($http, $scope){
+});
